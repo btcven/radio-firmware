67f99bb9ef800b699f9dff3da752f17f082e282d
gustavosinbandera1 <gustavosinbandera1@hotmail.com>
2020-02-03 23:40:47 -0500

    remove some unused variables

--- main.c
+++ main.c
@@ -4,15 +4,12 @@
  * @brief Main firmware file
  * @version 0.1
  * @date 2020-02-02
- * 
+ *
  * @copyright Copyright (c) 2020 Locha Mesh project developers
  * @license Apache 2.0, see LICENSE file for details
- * 
+ *
  */
 
-// #include <stdio.h>
-// #include <stdlib.h>
-// #include <string.h>
 #include <unistd.h> // for getting the pid
 
 #define ENABLE_DEBUG (1)
@@ -20,16 +17,8 @@
 #include <inttypes.h>
 
 #include "aodvv2.h"
-// #include "routingtable.h"
-
 #include "msg.h"
 #include "shell.h"
-
-
-
-// #include <arpa/inet.h>
-// #include <netinet/in.h>
-// #include <sys/socket.h>
 
 #include "eui64.h"
 #include "net/gnrc/ipv6/nib.h"
@@ -38,89 +27,40 @@
 #include "net/gnrc/ipv6/nib/nc.h"
 
 #include "net/gnrc.h"
-// #include "net/gnrc/pktdump.h"
-#include "net/netdev_test.h"
-#include "od.h"
-#include "xtimer.h"
-
-#include "net/gnrc/ipv6.h"
-
-#include "luid.h"
-#include "shell_functions.c"
 
 
-#include "shell.h"
 #include "msg.h"
-#include "net/ipv6/addr.h"
-#include "net/gnrc/pkt.h"
-#include "net/gnrc/pktbuf.h"
-#include "net/gnrc/netreg.h"
-#include "net/gnrc/netapi.h"
-#include "net/gnrc/netif.h"
-#include "net/gnrc/netif/conf.h"
-#include "net/gnrc/netif/ieee802154.h"
-#include "net/gnrc/netif/hdr.h"
-#include "net/gnrc/pktdump.h"
-#include "net/netdev_test.h"
+#include "shell.h"
+#include "shell_functions.c"
 #include "xtimer.h"
-
-
-
-// static gnrc_netreg_entry_t _dumper;
-// static msg_t _dumper_queue[DUMPER_QUEUE_SIZE];
-// static char _dumper_stack[THREAD_STACKSIZE_MAIN];
 
 extern void init_socket(void);
-extern int show_routingtable(int argc, char **argv);
 extern int demo_send(int argc, char **argv);
 extern int udp_cmd(int argc, char **argv);
-extern int demo_add_neighbor(int argc, char **argv);
 extern void *_demo_receiver_thread(void *arg);
-extern int list_neighbors(int argc, char **argv);
 
 const shell_command_t shell_commands[] = {
-    {"print_rt", "print routingtable", show_routingtable},
-    {"add_neighbor", "add neighbor to Neighbor Cache", demo_add_neighbor},
-    {"send", "send message to ip", demo_send},
-    {"list", "listar neighbors", list_neighbors},
-    {NULL, NULL, NULL}};
+    {"send", "send message to ip", demo_send}, {NULL, NULL, NULL}};
 
 int main(void) {
-  void *iter_state = NULL;
-  gnrc_ipv6_nib_nc_t nce;
 
   msg_init_queue(msg_q, RCV_MSG_Q_SIZE);
   init_socket();
   aodv_init();
 
-
-  (void)puts("Welcome to RIOT!\n");
+  puts("Welcome to RIOT!\n");
   _mock_netif = gnrc_netif_iter(_mock_netif);
+
   if (_mock_netif != NULL) {
-    DEBUG("tenemos una interface PID: %d\n", _mock_netif->pid);
+    DEBUG("interface PID: %d\n", _mock_netif->pid);
     ipv6_addr_t ipv6_addr;
-    int res = gnrc_netapi_get(_mock_netif->pid, NETOPT_IPV6_ADDR, 0,
+    gnrc_netapi_get(_mock_netif->pid, NETOPT_IPV6_ADDR, 0,
                               (void *)&ipv6_addr, sizeof(ipv6_addr));
 
     char addr_str[IPV6_ADDR_MAX_STR_LEN];
-     ipv6_addr_to_str(addr_str, &ipv6_addr, sizeof(ipv6_addr));
-    DEBUG("-------------------dst = %s",addr_str);
-    printf("***********la salida es %d\n", res);
+    ipv6_addr_to_str(addr_str, &ipv6_addr, sizeof(ipv6_addr));
+    DEBUG("-------------------dst = %s", addr_str);
   }
-
-  uint8_t hwaddr[GNRC_NETIF_L2ADDR_MAXLEN];
-  uint16_t hwaddr_len;
-
-  if (gnrc_netapi_get(_mock_netif->pid, NETOPT_SRC_LEN, 0, &hwaddr_len,
-                      sizeof(hwaddr_len)) < 0) {
-    return false;
-  }
-
-  luid_get(hwaddr, hwaddr_len);
-  for (unsigned int j = 0; j < GNRC_NETIF_L2ADDR_MAXLEN; j++) {
-    printf(" 0x%02x", hwaddr[j]);
-  }
-  printf("\n");
 
   // neighbor information base init
   gnrc_ipv6_nib_init();
@@ -128,12 +68,6 @@ int main(void) {
   // to bind desired iface to nib
   gnrc_ipv6_nib_init_iface(_mock_netif);
   gnrc_netif_release(_mock_netif);
-
-  // this should print the neighbord list but I cannnot see anything
-  while (gnrc_ipv6_nib_nc_iter(0, &iter_state, &nce)) {
-    gnrc_ipv6_nib_nc_print(&nce);
-  }
-  printf("LA IFACE ES: %u", gnrc_ipv6_nib_nc_get_iface(&nce));
 
   thread_create(_rcv_stack_buf, sizeof(_rcv_stack_buf), THREAD_PRIORITY_MAIN,
                 THREAD_CREATE_STACKTEST, _demo_receiver_thread, NULL,
--- shell_functions.c
+++ shell_functions.c
@@ -5,16 +5,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <unistd.h> // for getting the pid
+#include <unistd.h>
 
 #define ENABLE_DEBUG (1)
 #include "debug.h"
 #include <inttypes.h>
 
-#include "routingtable.h"
-
-#include "eui64.h"
 #include "net/gnrc/ipv6/nib.h"
+
+#include "constants.h"
 
 #include "net/gnrc/ipv6/nib.h"
 #include "net/gnrc/ipv6/nib/nc.h"
@@ -22,140 +21,16 @@
 #define DUMPER_QUEUE_SIZE (16)
 // constants from the AODVv2 Draft, version 03
 #define DISCOVERY_ATTEMPTS_MAX (1) //(3)
-#define RREQ_WAIT_TIME (2000000) 
+#define RREQ_WAIT_TIME (2000000)
 char _rcv_stack_buf[THREAD_STACKSIZE_MAIN];
 
-#define NBR_MAC                                                                \
-  { 0x57, 0x44, 0x33, 0x22, 0x11, 0x00, }
-#define NBR_LINK_LOCAL                                                         \
-  {                                                                            \
-    0xfe, 0x80, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x55, 0x44, 0x33, 0xff,    \
-        0xfe, 0x22, 0x11, 0x00,                                                \
-  }
-#define DST                                                                    \
-  {                                                                            \
-    0x20, 0x01, 0x0d, 0xb8, 0x00, 0x00, 0xab, 0xcd, 0x55, 0x44, 0x33, 0xff,    \
-        0xfe, 0x22, 0x11, 0x00,                                                \
-  }
-#define DST_PFX_LEN (64U)
-
-/* IPv6 header + payload:https://github.com/gustavosinbandera1/RIOT version+TC
- * FL: 0       plen: 16    NH:17 HL:64 */
-#define L2_PAYLOAD                                                             \
-  {                                                                            \
-    0x60, 0x00, 0x00, 0x00, 0x00, 0x10, 0x11,                                  \
-        0x40, /* source: random address */                                     \
-        0x20, 0x01, 0x0d, 0xb8, 0x00, 0x00, 0xef, 0x01, 0x02, 0xca, 0x4b,      \
-        0xef, 0xf4, 0xc2, 0xde, 0x01, /* destination: DST */                   \
-        0x20, 0x01, 0x0d, 0xb8, 0x00, 0x00, 0xab, 0xcd, 0x55, 0x44, 0x33,      \
-        0xff, 0xfe, 0x22, 0x11, 0x00, /* random payload of length 16 */        \
-        0x54, 0xb8, 0x59, 0xaf, 0x3a, 0xb4, 0x5c, 0x85, 0x1e, 0xce, 0xe2,      \
-        0xeb, 0x05, 0x4e, 0xa3, 0x85,                                          \
-  }
-
-
 #define RCV_MSG_Q_SIZE (64)
-msg_t msg_q[RCV_MSG_Q_SIZE];  
-
-// static char addr_str[IPV6_ADDR_MAX_STR_LEN];
-static const uint8_t _nbr_mac[] = NBR_MAC;
-static const ipv6_addr_t _nbr_link_local = {.u8 = NBR_LINK_LOCAL};
+msg_t msg_q[RCV_MSG_Q_SIZE];
 
 gnrc_netif_t *_mock_netif = NULL;
 struct sockaddr_in6 _sockaddr;
 static int _sock_snd;
 timex_t _now;
-
-
-int show_routingtable(int argc, char **argv) {
-  (void)argc;
-  (void)argv;
-  print_routingtable();
-  return 0;
-}
-
-
-
-int demo_add_neighbor(int argc, char **argv) {
-
-  (void)_nbr_link_local;
-  (void)argv;
-
-  if (argc != 3) {
-    printf("Usage: add_neighbor <neighbor ip> <neighbor ll-addr>\n");
-    // return 1;
-  }
-
-  _mock_netif = gnrc_netif_iter(_mock_netif);
-  if (_mock_netif != NULL) {
-    DEBUG("tenemos una interface PID: %d\n", _mock_netif->pid);
-  }
-
-  void *iter_state = NULL;
-  gnrc_ipv6_nib_nc_t nce;
-  int res;
-  (void)res;
-
-  ipv6_addr_t neighbor;
-  (void)neighbor;
-  (void)_nbr_mac;
-
-
-//Internet network address in its standard text presentation form into its
-  // numeric binary form.
-
-  //inet_pton(AF_INET6, (char*)&_nbr_link_local, &neighbor);
-  inet_pton(AF_INET6, argv[1], &neighbor);
-
-  /* define neighbor to forward to */
-  res = gnrc_ipv6_nib_nc_set(&_nbr_link_local, _mock_netif->pid,
-  _nbr_mac, sizeof(_nbr_mac));
-
-
-  //this loop should show the list of neighbors
-  while (gnrc_ipv6_nib_nc_iter(0, &iter_state, &nce)) {
-    gnrc_ipv6_nib_nc_print(&nce);
-  }
-
-  printf("la iface es %u", gnrc_ipv6_nib_nc_get_iface(&nce));
-  printf("el estado es %d", gnrc_ipv6_nib_nc_get_nud_state(&nce));
-
-  return 1;
-}
-
-
-
-int list_neighbors(int argc, char **argv) {
-
-  (void)_nbr_link_local;
-  (void)argv;
-  (void)argc;
-
-  _mock_netif = gnrc_netif_iter(_mock_netif);
-  if (_mock_netif != NULL) {
-    DEBUG("netif->PID: %d\n", _mock_netif->pid);
-  }
-
-  void *iter_state = NULL;
-  gnrc_ipv6_nib_nc_t nce;
-  int res;
-  (void)res;
-
-  ipv6_addr_t neighbor;
-  (void)neighbor;
-  (void)_nbr_mac;
-
-  //this loop should show the list of neighbors
-  while (gnrc_ipv6_nib_nc_iter(0, &iter_state, &nce)) {
-    gnrc_ipv6_nib_nc_print(&nce);
-  }
-
-  printf("iface: %u", gnrc_ipv6_nib_nc_get_iface(&nce));
-  printf("state: %d", gnrc_ipv6_nib_nc_get_nud_state(&nce));
-
-  return 1;
-}
-
 
 int demo_attempt_to_send(char *dest_str, char *msg) {
   uint8_t num_attempts = 0;
@@ -197,7 +72,6 @@ int demo_attempt_to_send(char *dest_str, char *msg) {
   return -1;
 }
 
-
 int demo_send(int argc, char **argv) {
   if (argc != 3) {
     printf("Usage: send <destination ip> <message>\n");
@@ -210,7 +84,6 @@ int demo_send(int argc, char **argv) {
   return demo_attempt_to_send(dest_str, msg);
 }
 
-
 void init_socket(void) {
   _sockaddr.sin6_family = AF_INET6;
   _sockaddr.sin6_port = (int)htons(MANET_PORT);
@@ -222,7 +95,6 @@ void init_socket(void) {
     return;
   }
 }
-
 
 void *_demo_receiver_thread(void *args) {
   (void)args;
