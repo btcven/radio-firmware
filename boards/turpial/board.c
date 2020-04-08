ea8414f9cfa416d0a5eb502366f8e6683894f7ea
Jean Pierre Dudey <jeandudey@hotmail.com>
2020-04-08 14:13:36 -0500

    boards: add "turpial" board configuration files
    Signed-off-by: Jean Pierre Dudey <jeandudey@hotmail.com>

---
+++ boards/turpial/board.c
@@ -0,0 +1,33 @@
+/*
+ * Copyright (C) 2020 Locha Inc
+ *
+ * This file is subject to the terms and conditions of the GNU Lesser General
+ * Public License v2.1. See the file LICENSE in the top level directory for more
+ * details.
+ */
+
+/**
+ * @ingroup         boards_turpial
+ * @{
+ *
+ * @file
+ * @brief           Board specific implementations for Locha Mesh Turpial
+ *
+ */
+
+#include "cpu.h"
+#include "board.h"
+
+/**
+ * @brief           Initialise the board.
+ */
+
+void board_init(void)
+{
+    cpu_init();
+
+    if (MIMIC_LAUNCHPAD) {
+        gpio_init(LED0_PIN, GPIO_OUT);
+        gpio_init(LED1_PIN, GPIO_OUT);
+    }
+}
