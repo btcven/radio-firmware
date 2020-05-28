815333b04b726cfaa3f21171fd6fddd68f0428b6
Jean Pierre Dudey <jeandudey@hotmail.com>
2020-05-28 12:36:53 -0500

    turpial-firmware: refactor netdev init
    Signed-off-by: Jean Pierre Dudey <jeandudey@hotmail.com>

--- main.c
+++ main.c
@@ -29,118 +29,102 @@
 
 #include "net/aodvv2/aodvv2.h"
 #include "net/manet/manet.h"
-
-#if IS_USED(MODULE_VAINA)
 #include "net/vaina.h"
-#endif
 
-#if IS_USED(MODULE_SHELL_EXTENDED)
 #include "shell_extended.h"
-#endif
 
-#ifndef CONFIG_SLIP_LOCAL_ADDR
-/**
- * @brief   SLIP link local address
- */
-#define CONFIG_SLIP_LOCAL_ADDR "fe80::dead:beef:cafe:babe"
-#endif
+static int _init_ieee802154(void);
+static int _init_slipdev(void);
 
 /**
- * @brief   Find a network interface.
+ * @brief   Network interfaces PID.
  *
- * @param[in] nettype The network type of the interface to find.
+ * These are the available network interfaces on the Turpial device, please
+ * note that these values _aren't_ fixed, they _might_ and _will_ change in
+ * the future, so don't worry if on the console you get various errors, just
+ * update accordingly, this happens because there's still an ongoing process
+ * on RIOT to add identifiers to network interfaces so this doesn't has to be
+ * done manually in the future.
  *
- * @return The gnrc_netif_t network interface.
- * @retval NULL if no interface was found.
+ * If something has it's own thread and is created before these network
+ * interfaces you might want to update them with a fast `ifconfig` and replacing
+ * the values yourself here ;).
+ *
+ * @{
  */
-static gnrc_netif_t *_find_netif(uint16_t nettype);
+#define IEEE802154_IF (8)
+#define SLIPDEV_IF    (7)
+/** @} */
 
 int main(void)
 {
-    gnrc_netif_t *ieee802154_netif = _find_netif(NETDEV_TYPE_IEEE802154);
-
-    /* Join LL-MANET-Routers multicast group */
-    if (manet_netif_ipv6_group_join(ieee802154_netif) < 0) {
-        printf("Couldn't join MANET mcast group\n");
+    if (_init_ieee802154() < 0) {
+        printf("Error: Couldn't initialize IEEE 802.15.4g device correctly.\n");
     }
 
-    /* Add global address */
-    eui64_t iid;
-    if (gnrc_netif_ipv6_get_iid(ieee802154_netif, &iid) == sizeof(uint64_t)) {
-        ipv6_addr_t addr = {{ 0x20, 0x01, 0x00, 0x00,
-                              0x00, 0x00, 0x00, 0x00,
-                              0x00, 0x00, 0x00, 0x00,
-                              0x00, 0x00, 0x00, 0x00 }};
-        ipv6_addr_init_iid(&addr, (uint8_t *)&iid, 64);
-
-        if (gnrc_netif_ipv6_addr_add(ieee802154_netif, &addr, 128, 0) != sizeof(ipv6_addr_t)) {
-            printf("Couln't setup global address\n");
-        }
+    if (_init_slipdev() < 0) {
+        printf("Error: Couldn't initialize SLIP correctly.\n");
+        return -1;
     }
 
-    /* Initialize RFC5444 */
-    if (aodvv2_init(ieee802154_netif) < 0) {
-        printf("Couldn't initialize RFC5444\n");
-    }
+    puts("Welcome to Turpial Radio!");
 
-#if IS_USED(MODULE_VAINA)
-    gnrc_netif_t *slipdev_netif = _find_netif(NETDEV_TYPE_SLIP);
-    printf("found SLIP netif %d\n", slipdev_netif->pid);
-    if (slipdev_netif == NULL) {
-        printf("VAINA needs a wired interface (SLIP) to work!\n");
-    }
-    else {
-        ipv6_addr_t addr;
-        if (ipv6_addr_from_str(&addr, CONFIG_SLIP_LOCAL_ADDR) == NULL) {
-            printf("Malformed SLIP local address, please verify it!\n");
-        }
-
-        if (gnrc_netif_ipv6_addr_add(slipdev_netif, &addr, 128, 0) != sizeof(ipv6_addr_t)) {
-            printf("Couldn't setup SLIP local address\n");
-        }
-
-        if (vaina_init(slipdev_netif) < 0) {
-            printf("Couldn't initialize VAINA\n");
-        }
-    }
-#endif
-
-    puts("Welcome to Turpial CC1312 Radio!");
-
-    char line_buf[SHELL_DEFAULT_BUFSIZE];
     /* Start shell */
-#if IS_USED(MODULE_SHELL_EXTENDED)
+    char line_buf[SHELL_DEFAULT_BUFSIZE];
     shell_run(shell_extended_commands, line_buf, SHELL_DEFAULT_BUFSIZE);
-#else
-    shell_run(NULL, line_buf, SHELL_DEFAULT_BUFSIZE);
-#endif
 
     /* Should be never reached */
     return 0;
 }
 
-static gnrc_netif_t *_find_netif(uint16_t nettype)
+static int _init_ieee802154(void)
 {
-    static uint16_t device_type = 0;
-
-    static gnrc_netapi_opt_t opt = {
-        .opt = NETOPT_DEVICE_TYPE,
-        .context = 0,
-        .data = &device_type,
-        .data_len = sizeof(uint16_t),
-    };
-
-    /* Iterate over network interfaces and find one that matches */
-    gnrc_netif_t *netif = NULL;
-    for (netif = gnrc_netif_iter(netif);
-         netif != NULL;
-         netif = gnrc_netif_iter(netif)) {
-        if (gnrc_netif_get_from_netdev(netif, &opt) == sizeof(uint16_t)) {
-            if (device_type == nettype) {
-                return netif;
-            }
-        }
+    gnrc_netif_t *ieee802154_netif = gnrc_netif_get_by_pid(IEEE802154_IF);
+    if (ieee802154_netif == NULL) {
+        printf("Error: Couldn't find IEEE 802.154.g device.\n");
+        return -1;
     }
 
-    return NULL;
+    /* Join LL-MANET-Routers multicast group, this is the IPv6 group where we'll
+     * be receiving RFC 5444 UDP packets */
+    if (manet_netif_ipv6_group_join(ieee802154_netif) < 0) {
+        printf("Error: Couldn't join LL-MANET-Routers group\n");
+        return -1;
+    }
+
+    /* Initialize AODVv2 */
+    if (aodvv2_init(ieee802154_netif) < 0) {
+        printf("Error: Couldn't initialize AODVv2\n");
+        return -1;
+    }
+
+    return 0;
+}
+
+static int _init_slipdev(void)
+{
+    gnrc_netif_t *slipdev_netif = gnrc_netif_get_by_pid(SLIPDEV_IF);
+    if (slipdev_netif == NULL) {
+        printf("Error: We need a wired interface to send packets to.\n");
+        return -1;
+    }
+
+    /* Add known fe80::2 local address so a computer/esp32 can know how to
+     * configure the routing table. */
+    ipv6_addr_t addr = {
+        .u8 = { 0xfe, 0x80, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2 }
+    };
+
+    if (gnrc_netif_ipv6_addr_add(slipdev_netif, &addr, 128, 0) != sizeof(ipv6_addr_t)) {
+        printf("Error: Couldn't setup SLIP local address.\n");
+        return -1;
+    }
+
+    /* Initialize VAINA config interface */
+    if (vaina_init(slipdev_netif) < 0) {
+        printf("Error: Couldn't initialize VAINA\n");
+        return -1;
+    }
+
+    return 0;
 }
