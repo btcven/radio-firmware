def65cd4cf98b23fe65c0aa7400cd247bb21e13e
Jean Pierre Dudey <jeandudey@hotmail.com>
2020-06-01 15:15:18 -0500

    aodvv2: fix IPv6 prefix match bug
    Signed-off-by: Jean Pierre Dudey <jeandudey@hotmail.com>

--- sys/net/aodvv2/aodvv2_rcs.c
+++ sys/net/aodvv2/aodvv2_rcs.c
@@ -54,8 +54,8 @@ aodvv2_rcs_entry_t *aodvv2_rcs_add(const ipv6_addr_t *addr, uint8_t pfx_len,
     }
 
     const aodvv2_rcs_entry_t *entry = aodvv2_rcs_matches(addr, pfx_len);
-    if (entry) {
-        DEBUG_PUTS("aodvv2: client exists, not adding it.\n");
+    if (entry != NULL) {
+        DEBUG_PUTS("aodvv2: client exists, not adding it");
         return NULL;
     }
 
@@ -76,7 +76,7 @@ aodvv2_rcs_entry_t *aodvv2_rcs_add(const ipv6_addr_t *addr, uint8_t pfx_len,
         }
     }
 
-    DEBUG_PUTS("aodvv2: router client set is full.\n");
+    DEBUG_PUTS("aodvv2: router client set is full");
     mutex_unlock(&_lock);
     return NULL;
 }
@@ -120,7 +120,7 @@ aodvv2_rcs_entry_t *aodvv2_rcs_matches(const ipv6_addr_t *addr,
         /* Compare addresses by prefix */
         if ((entry->data.pfx_len == pfx_len) &&
             (ipv6_addr_match_prefix(&entry->data.addr,
-                                   addr) == entry->data.pfx_len)) {
+                                   addr) >= entry->data.pfx_len)) {
             mutex_unlock(&_lock);
             return &entry->data;
         }
@@ -144,7 +144,7 @@ bool aodvv2_rcs_is_client(const ipv6_addr_t *addr)
 
         /* Compare addresses by prefix */
         if (ipv6_addr_match_prefix(&entry->data.addr,
-                                   addr) == entry->data.pfx_len) {
+                                   addr) >= entry->data.pfx_len) {
             mutex_unlock(&_lock);
             return true;
         }
@@ -165,8 +165,8 @@ void aodvv2_rcs_print_entries(void)
             continue;
         }
 
-        /* prints ipv6/prefix - cost */
-        printf("%s/%u - %u",
+        /* prints ipv6/prefix | cost */
+        printf("%s/%u | %u\n",
                ipv6_addr_to_str(buf, &entry->data.addr, sizeof(buf)),
                entry->data.pfx_len, entry->data.cost);
     }
